d124ac66c319fae7a60bca96732dd293e5d93dc4
Christophe Eymard <christophe.eymard@sales-way.com>
2016-08-05 15:50:32 +0200

    removed requestAnimationFrame that was used for when 'mount' wasn't triggered properly

--- src/map.ts
+++ src/map.ts
@@ -88,12 +88,12 @@ export class MapCtrl extends Controller {
     }).addTo(this.l);
 
     // Enfin, un petit fix pour être sûr que la map calcule bien sa propre taille.
-    requestAnimationFrame(() => {
+    // requestAnimationFrame(() => {
 			// this.l.setView({lat: 45.54578, lng: 5.342374}, 7)
 			for (let fn of this._queue)
 				fn(this.l)
 			this._queue = []
-    });
+    // });
 
 	}
 
